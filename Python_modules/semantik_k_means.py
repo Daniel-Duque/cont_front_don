b1e8c712a2479d2e6f55d544412410ff75bc27f9
daniel <dduque80@hotmail.com>
2025-03-12 11:35:15 -0500

    Update semantik_k_means.py

--- Python_modules/semantik_k_means.py
+++ Python_modules/semantik_k_means.py
@@ -12,15 +12,24 @@ import os
 from sentence_transformers import SentenceTransformer
 from sentence_transformers import util
 
-def semantic_search(prompt, df, model, top_k=1000):
+def semantic_k(prompt, df, model,similitud_min=0.4,k=20 ):
     # Generate embeddings for the descriptions
     df['embeddings'] = df['Descripcion del Proceso'].apply(lambda x: model.encode(x, convert_to_tensor=True))
-
+    print("a")
     prompt_embedding = model.encode(prompt, convert_to_tensor=True)
     similarities = df['embeddings'].apply(lambda x: util.pytorch_cos_sim(prompt_embedding, x).item())
     df['similarity'] = similarities
-    return df
+    accept=df[df['similarity'] >similitud_min].sort_values("similarity",ascending=False)
+    mean=accept.iloc[0:k]["value_thousand_dolar"].mean()
+    desv=accept.iloc[0:k]["value_thousand_dolar"].std()
+    kurt=accept.iloc[0:k]["value_thousand_dolar"].kurtosis()
+    
+    return mean,desv,kurt
 
-
-
+model = SentenceTransformer('tomaarsen/static-similarity-mrl-multilingual-v1')
+prompt = "construcción de hospitales"
 data1=pd.read_csv(r"data/compilado_error.csv",encoding="utf-8",delimiter=";")
+df=data1[0:100000]
+mean,desv,kurt=semantic_k(prompt, df, model,k=20)
+rangop=mean+desv-kurt
+rangon=max(mean-desv-kurt,0)
